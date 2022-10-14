85f570f7e2eb7d3ee10dd0d3fdf043ed9b6633b0
Aleksandre-sco <aleksandre.kapanadze.2@btu.edu.ge>
2022-10-14 16:01:47 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 12;
+        int c = 18;
+
+        System.out.println(b*b - (4*a*c));
+
+    }
+}
